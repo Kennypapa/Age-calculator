25e1c34ce8c510220792e320baeee19384429a50
Kennypapa <daramolakehinde339@gmail.com>
2022-12-26 14:26:21 -0800

    hert

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-var dateInput = document.getElementById('date').value;
-var monthInput = document.getElementById('month').value;
-var yearInput = document.getElementById('year').value; 
-
-var currentDate = new Date();
-var d2 = currentDate.getDate();
-var m2 = currentDate.getMonth() + 1;
-var y2 = currentDate.getFullYear();
-
 const age = () => {
-    var  d = d2 - dateInput;  
-    var  m = m2 - monthInput;
-    var  y = y2 - yearInput;
+    var dateInput = document.getElementById('date').value;
+    var monthInput = document.getElementById('month').value;
+    var yearInput = document.getElementById('year').value; 
 
+    var newDate = new Date();
+    var currentDate = newDate.getDate();
+    var currentMonth = newDate.getMonth() + 1;
+    var currentYear = newDate.getFullYear();
 
+    var  d = currentDate - dateInput;  
+    var  m = currentMonth - monthInput;
+    var  y = currentYear - yearInput;
+
+    if(yearInput > currentYear) { 
+        alert('Invalid year Input');
+    }else if(monthInput > 13){
+        alert('Invalid month Input');
+    }else if(dateInput > 31){
+        alert('Date should not be more than 31');
+    }
+   
     document.getElementById('age').innerHTML = 'Your Age is ' +y+ ' Years '+m+' Months '+d+' Days'; 
-    console.log(m2)
 }
